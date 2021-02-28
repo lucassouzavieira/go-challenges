216f2bfc0b8d9b34a05b90c487f6aeb0ebdf7490
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2021-02-28 11:09:55 -0300

    Add Quiz with time limit

--- 2-QuizExercise/main.go
+++ 2-QuizExercise/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 )
 
 type Question struct {
@@ -15,7 +16,7 @@ func (q Question) CheckAnswer(aswer string) bool {
 	return q.Answer == aswer
 }
 
-func main() {
+func Quiz() {
 	fmt.Println("Quiz Game!")
 	fmt.Println("Answer the following questions")
 
@@ -62,4 +63,66 @@ func main() {
 	}
 
 	fmt.Println("You answered " + fmt.Sprint(correct) + " questions correctly!")
+}
+
+func QuizWithTime(max int) {
+	start := time.Now()
+
+	fmt.Println("Quiz Game!")
+	fmt.Println("Answer the following questions")
+
+	// Open CSV file
+	csvFile, err := os.Open("problems.csv")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Close CSV file at the end of the program
+	defer csvFile.Close()
+
+	reader, err := csv.NewReader(csvFile).ReadAll()
+
+	if err != nil {
+		fmt.Println("Failed to load CSV file")
+	}
+
+	// Correct answers
+	var correct int = 0
+
+	for index, line := range reader {
+		// Skip CSV headers
+		if index == 0 {
+			continue
+		}
+
+		question := Question{
+			Question: line[0],
+			Answer:   line[1],
+		}
+
+		fmt.Println(question.Question)
+
+		var answer string
+		fmt.Scan(&answer)
+
+		if question.CheckAnswer(answer) {
+			correct = correct + 1
+		}
+
+		fmt.Println("")
+
+		now := time.Now()
+		diff := now.Sub(start)
+
+		if diff.Seconds() >= float64(max) {
+			break
+		}
+	}
+
+	fmt.Println("You answered " + fmt.Sprint(correct) + " questions correctly!")
+}
+
+func main() {
+	QuizWithTime(20)
 }
